1a3378f7f175b10964fee0e9817c99f9f6e3053f
Adetunji <ashennaike@gmail.com>
2019-05-06 19:14:46 -0400

    done with mvp

--- route/userRoute.js
+++ route/userRoute.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 //import database
-const db = require('../data/dbConfig');
 const Users = require('../data/models/userModel')
+
+const server = express();
 
 //import and set middleware
 const permission = require('../auth/require_permission');
@@ -24,7 +25,7 @@ const newError = (sts, msg, res) => {
 
 //CRUD statements
 //get request
-sever.get('/users', permission, (req, res) => {
+server.get('/users', permission, (req, res) => {
   Users.get()
   .then(users => {
     res.status(200).json(users);
@@ -36,12 +37,40 @@ sever.get('/users', permission, (req, res) => {
 
 //register 
 server.post('api/register', ( req, res ) => {
+  const user = req.body;
 
+  const hash = bcrypt.hashSync(user.password, 10);
+
+  user.password = hash;
+
+  Users
+  .add(user)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch( err => {
+    return sendError( err, res );
+  })
 })
 
 //login
 server.post('/login', (req, res) => {
-  
+  const { username, password } = req.body;
+
+  Users
+  .idCheck({ username })
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      next();
+    }
+    else {
+      res.status(401).json({ message: 'sorry bro its wrong ' })
+    }
+  })
+  .catch( err => {
+    res.status(500).json( err )
+  })
+
 })
 
 
